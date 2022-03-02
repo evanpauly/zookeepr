dead5d360b1b0189d96ebca960346f24ca7e30b9
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-03-02 10:05:31 -0600

    third part done

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+
+app.use(express.static('./public/zookeepr-public'));
+
 const { animals } = require('./data/animals');
+
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -107,6 +111,22 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+    });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepr-public/index.html'));
+    });
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepr-public/animals.html'));
+    });
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepr-public/zookeepers.html'));
+    });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepr-public/index.html'));
     });
 
 app.listen(PORT, () => {
